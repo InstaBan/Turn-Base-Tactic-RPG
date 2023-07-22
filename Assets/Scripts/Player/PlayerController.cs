b62e2e78f07a14f2b39b55326d376ede11b53ae2
InstaBan <101858553+InstaBan@users.noreply.github.com>
2023-07-23 04:45:50 +1000

    Sample Scene for Combat

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,62 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using FishNet.Connection;
 using FishNet.Object;
 using UnityEngine;
 
-public class PlayerController : NetworkBehaviour
+namespace LuminaStudio.Player
 {
-    [SerializeField]
-    private float m_movementSpeed = 5f;
-    [SerializeField]
-    private float m_rotationSpeed = 180f;
-    [SerializeField]
-    private float m_cameraFollowDistanceX = 0f;
-    [SerializeField]
-    private float m_cameraFollowDistanceY = 0f;
-    [SerializeField]
-    private float m_cameraFollowDistanceZ = -3f;
-    private Camera m_playerCamera;
-    CharacterController characterController;
-    public override void OnStartClient()
+    public class PlayerController : NetworkBehaviour
     {
-        base.OnStartClient();
-        if (base.IsOwner) 
+        [SerializeField]
+        private float m_movementSpeed = 5f;
+        [SerializeField]
+        private float m_rotationSpeed = 180f;
+        [SerializeField]
+        private float m_cameraFollowDistanceX = 0f;
+        [SerializeField]
+        private float m_cameraFollowDistanceY = 0f;
+        [SerializeField]
+        private float m_cameraFollowDistanceZ = -3f;
+        private Camera m_playerCamera;
+        CharacterController characterController;
+        public override void OnStartClient()
         {
-            m_playerCamera = Camera.main;
-            m_playerCamera.transform.SetParent(transform);
+            base.OnStartClient();
+            if (base.IsOwner) 
+            {
+                m_playerCamera = Camera.main;
+                m_playerCamera.transform.SetParent(transform);
 
-            m_playerCamera.transform.localPosition = new Vector3(0f, 2f, -5f);
-            m_playerCamera.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
-            Vector3 lookDirection = transform.forward;
-            m_playerCamera.transform.LookAt(transform.position + lookDirection);
+                m_playerCamera.transform.localPosition = new Vector3(0f, 2f, -5f);
+                m_playerCamera.transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
+                Vector3 lookDirection = transform.forward;
+                m_playerCamera.transform.LookAt(transform.position + lookDirection);
+            }
+            else 
+            {
+                gameObject.GetComponent<PlayerController>().enabled = false;
+            }
         }
-        else 
+        void Start()
         {
-            gameObject.GetComponent<PlayerController>().enabled = false;
-        }
-    }
-    void Start()
-    {
         
-    }
+        }
 
-    private void Update()
-    {
-        Movement();
-    }
+        private void Update()
+        {
+            Movement();
+        }
 
-    private void Movement()
-    {
-        // translate
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * m_movementSpeed * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
+        private void Movement()
+        {
+            // translate
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * m_movementSpeed * Time.deltaTime;
+            transform.Translate(movement, Space.Self);
 
-        // rotate
-        float mouseX = Input.GetAxis("Mouse X");
-        float rotationAmount = mouseX * m_rotationSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.up, rotationAmount);
+            // rotate
+            float mouseX = Input.GetAxis("Mouse X");
+            float rotationAmount = mouseX * m_rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, rotationAmount);
+        }
     }
 }
